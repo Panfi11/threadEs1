73f09c0296dc5ded726b52628ac11e732be385d8
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-05-07 09:03:18 +0200

    Add project files.

---
+++ threadEs1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace threadEs1
+{
+    class Program
+    {
+        public static void StampaNumeri(bool pari)
+        {
+            Console.WriteLine("ID Thread: " + Thread.CurrentThread.ManagedThreadId);
+            if (pari == true)
+            {
+                StampaNumeriPari();
+            }
+            else
+            {
+                StampaNumeriDispari();
+
+            }
+        }
+
+        public static void StampaNumeriDispari()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 1)
+                    Console.WriteLine($"Er numero {i} è DISPARI.");
+            }
+        }
+
+        public static void StampaNumeriPari()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine($"Er numero {i} è PARI.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+                Thread thread = new Thread(() => StampaNumeri(true));
+                thread.Start();
+
+                StampaNumeri(false);
+
+                Console.ReadLine();
+        }
+    }
+}
